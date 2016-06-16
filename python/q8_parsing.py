8c50b76713d875fe4e098168f2690a4d3e5cf94e
Marwin Ko <marwinko19@gmail.com>
2016-06-15 17:16:32 -0700

    Update q8_parsing.py

--- python/q8_parsing.py
+++ python/q8_parsing.py
@@ -3,4 +3,11 @@
 # goals scored for and against each team in that season (so Arsenal scored 79 goals 
 # against opponents, and had 36 goals scored against them). Write a program to read the file, 
 # then print the name of the team with the smallest difference in ‘for’ and ‘against’ goals.
-
+import numpy as np
+data = np.genfromtxt('football.csv',delimiter=',',dtype='S15,int,int,int,int,int,int,int')
+temp = []
+for i,x in enumerate(data):
+    info = (x[0],x[5]-x[6])
+    temp.append(info)
+temp = sorted(temp,key=lambda x:x[1])
+print 'The team with the smallest point differential is',temp[0][0]
