6b6c54050ac2d68e5b8573555ffcc193b003cfb1
Marwin Ko <marwinko19@gmail.com>
2016-06-17 16:11:09 -0700

    Update advanced_python_csv.py
    @sampathweb

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,1 +1,8 @@
-PLACE YOUR CODE HERE
+import numpy as np
+import csv
+data = np.genfromtxt('faculty.csv',delimiter=',',dtype='S20,S20,S20,S20')
+emails  = [data[i][3] for i in range(1,len(data))]
+with open('emails.csv','wb') as f:
+    w = csv.writer(f)
+    for email in emails:
+        w.writerow([email])
