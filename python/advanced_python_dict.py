fbb40d3931712fce5e64ae2937f5db0f4d7e9b21
Marwin Ko <marwinko19@gmail.com>
2016-06-17 16:44:34 -0700

    Update advanced_python_dict.py
    @sampathweb

--- python/advanced_python_dict.py
+++ python/advanced_python_dict.py
@@ -1,1 +1,18 @@
-PLACE YOUR CODE HERE
+import numpy as np
+data = np.genfromtxt('faculty.csv',delimiter=',',dtype='S20,S20,S20,S20')
+info = [[data[i][1],data[i][2],data[i][3]] for i in range(1,len(data))]
+names = [data[i][0] for i in range(1,len(data))]
+#####################################  Q6  #####################################
+last_name = [name.split(' ')[-1] for name in names]
+faculty_dict = dict(zip(last_name,info))
+print 'Question #6', faculty_dict
+print
+#####################################  Q7  #####################################
+full_name = [(name.split(' ')[0],name.split(' ')[-1]) for name in names]
+faculty_dict = dict(zip(full_name,info))
+print 'Question #7', faculty_dict
+print
+#####################################  Q8  #####################################
+sort_last = sorted(full_name, key=lambda x:x[-1] )
+for name in sort_last:
+    print name,':',faculty_dict[name]
